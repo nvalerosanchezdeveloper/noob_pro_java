70b2ca1e455f290b07d6b2703975f56722d1f94f
nvalerosanchezdeveloper <tu_email@example.com>
2026-04-15 18:19:22 +0200

    Mejorar Ejercicio 1.2: usar printf y agregar cálculo de rectángulos que caben en 100 unidades cuadradas

--- ejercicios/Ejercicio_1_2/src/Main.java
+++ ejercicios/Ejercicio_1_2/src/Main.java
@@ -1,6 +1,6 @@
 //        EJERCICIO 1.2: Escribe un programa que calcule el área y perímetro de un rectángulo dado
-//        su base (8.5) y altura (4.2). Muestra los resultados con dos decimales usando printf.También calcula cuántos
-//        rectángulos enteros caben en un espacio de 100 unidades cuadradas (usa división entera).
+//        su base (8.5) y altura (4.2). Muestra los resultados con dos decimales usando printf.
+//        También calcula cuántos rectángulos enteros caben en un espacio de 100 unidades cuadradas (usa división entera).
 
 
 public class Main {
@@ -11,8 +11,11 @@ public class Main {
         double area = base * height;
         double perimeter = 2*(base + height);
 
-        System.out.print("Área del rectángulo: " + area + "\n" +
-                         "Perímetro del rectángulo: " + perimeter);
+        System.out.printf("Área del rectángulo: %2f%n" +
+                         "Perímetro del rectángulo: %2f%n", area, perimeter);
+
+        int rectangles = (int)(100/area);
+        System.out.printf("Número de rectángulos que caben: %d%n", rectangles);
 
 
 
