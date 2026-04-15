63f3446fb2226ed997f92649612d11a58c902d63
nvalerosanchezdeveloper <tu_email@example.com>
2026-04-15 16:35:52 +0200

    Agregar ejercicio_1.1 con clases Main e in

---
+++ ejercicios/ejercicio_1.1/src/Main.java
@@ -0,0 +1,16 @@
+//EJERCICIO 1.1: Crea un programa que imprima tu nombre, tu edad y tu objetivo con Java, cada dato en una línea diferente.
+//Compílalo y ejecútalo desde la terminal sin IDE.
+//Experimenta con println, print y printf.
+
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "Nicolás";
+        int age = 26;
+        System.out.printf("Nombre: "+ name+ "\n" +
+                          "Edad: "  + age + "\n" +
+                          "Objetivo: Masterizar Java\n");
+
+
+    }
+}
