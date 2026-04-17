ebe7b9c871812e287b6863e05a9934e821dde694
nvalerosanchezdeveloper <tu_email@example.com>
2026-04-17 23:05:56 +0200

    Actualización de cambios en Main.java y agregación de noob_pro_java.pdf

--- ejercicios/Ejercicio_1_5/src/Main.java
+++ ejercicios/Ejercicio_1_5/src/Main.java
@@ -3,16 +3,26 @@
 //repitiendo hasta que el array esté ordenado.
 //Muestra el array después de cada "pasada" completa para ver el progreso.
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
 
         int [] numbers = {64, 34, 25, 12, 22, 11, 90};
 
-        for (int i = 0; i <= 5; i++) {
-            for (int j = 0; j < i-1; j++) {
+        for (int i = 0; i < numbers.length-1; i++) {
+            for (int j = 0; j < numbers.length-1; j++) {
 
+                if (numbers [j] > numbers [j+1]){
+
+                    int aux = numbers [j];
+                    numbers [j] = numbers [j+1];
+                    numbers [j+1] = aux;
+
+                }
             }
+            System.out.println(Arrays.toString(numbers));
         }
     }
 }
