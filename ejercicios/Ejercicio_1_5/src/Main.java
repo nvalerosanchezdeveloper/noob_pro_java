13f84723078f7eb6ecb3c0d42882dd551b60f3d9
nvalerosanchezdeveloper <tu_email@example.com>
2026-04-17 07:22:48 +0200

    Add Exercise 1.5 and IDE configuration files

---
+++ ejercicios/Ejercicio_1_5/src/Main.java
@@ -0,0 +1,18 @@
+//EJERCICIO 1.5: Sin usar Arrays.sort(), implementa el algoritmo Bubble Sort manualmente sobre este array:
+//{64, 34, 25, 12, 22, 11, 90}. El Bubble Sort compara pares adyacentes y los intercambia si están en el orden incorrecto,
+//repitiendo hasta que el array esté ordenado.
+//Muestra el array después de cada "pasada" completa para ver el progreso.
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int [] numbers = {64, 34, 25, 12, 22, 11, 90};
+
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j < i-1; j++) {
+
+            }
+        }
+    }
+}
